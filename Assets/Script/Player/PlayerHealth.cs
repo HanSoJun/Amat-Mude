775c027a6c9d1926847add1191fd2ef27eea33da
HanSoJun <hansokya@gmail.com>
2022-05-21 12:00:12 +0700

    update

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class PlayerHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(0);
         }
     }
     // Update is called once per frame
